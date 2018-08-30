eaff3bd3f372900d5e04badeb2db02cf0559f76d
Soumya Nayak (Wipro Ltd.) <v-sonaya@microsoft.com>
2018-08-30 18:36:00 +0530

    added_pwdLoginFunction_in_testLoop.py

--- testLoops.py
+++ testLoops.py
@@ -10,6 +10,24 @@
 # #     l.append(int(input("num : ")))
 # # print(*[l[i]+l[i+1] for i in range(4)] , sep="\n" )
 # # print(*[l[i]+l[i+1] for i in range(4)] , sep="\t" )
+def login():
+    i='1'
+    while(i!='4'):
+        i=input("\n1: Home    2: MyProfile    3:ContactUs     4: LogOut\n")
+        if(i=='1'):
+            print("\n------------\nWelcome to Home!\n")
+        else:
+            if(i=='2'):
+                print("\n-------------\nMyProfile\n")
+                print("Name: xyz\nID: 123\nProject: Python")
+            else:
+                if(i=='3'):
+                    print("\n-------------\nthanks for support!\nPh: +1-123-123123\nemail:support@123.co\n")
+                else:
+                    if(i=='4'):
+                        print("\nLogging out!\n")
+                    else:
+                        print("\nWrong/Invalid input. Try again!\n")
 
 pwd=''
 while(pwd!='taopypy'):
@@ -19,13 +37,4 @@ while(pwd!='taopypy'):
         login()
     else:
         print("try again!")
-
-def login():
-    i=1
-    while(i!=4):
-        i=int(input("1: Home    2: MyProfile    3:ContactUs     4: Quit"))
-        if(i==1):
-        if(i==2):
-        if(i==3):
-        if(i<1 and i>4):
-            print("Wrong/Invalid input. Try again!")
+print("----EXIT----")
