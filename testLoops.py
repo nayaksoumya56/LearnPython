7fe8e4c3196115d8c9408c657535aeac251ddb1c
Soumya Nayak (Wipro Ltd.) <v-sonaya@microsoft.com>
2018-08-31 14:50:35 +0530

    added_p6.py

--- testLoops.py
+++ testLoops.py
@@ -1,15 +1,16 @@
-# email=["ftsud@nj.com","something@gmail.com",'whyso@serious.com']
-# for  item in email:# 3 lines of code
-#     if 'gmail' in item:
-#         print (item)
-# print(*[item for item in email if 'nj' in item]) # one line of code
-# print(email)
-# ##########################################################
-# # l=[]*5
-# # for i in range(5):
-# #     l.append(int(input("num : ")))
-# # print(*[l[i]+l[i+1] for i in range(4)] , sep="\n" )
-# # print(*[l[i]+l[i+1] for i in range(4)] , sep="\t" )
+email=["ftsud@nj.com","something@gmail.com",'whyso@serious.com']
+for  item in email:# 3 lines of code
+    if 'gmail' in item:
+        print (item)
+print(*[item for item in email if 'nj' in item]) # one line of code
+print(email)
+##########################################################
+l=[]*5
+for i in range(5):
+    l.append(int(input("num : ")))
+print(*[l[i]+l[i+1] for i in range(4)] , sep="\n" )
+print(*[l[i]+l[i+1] for i in range(4)] , sep="\t" )
+#############################################################
 def login():
     i='1'
     while(i!='4'):
